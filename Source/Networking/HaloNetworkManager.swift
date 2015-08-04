890a4fcf83b627615eb134ff54ce775c318e9cba
Borja Santos-Díez <borja.santos@mobgen.com>
2015-08-04 12:50:03 +0200

    Better implementation of the authentication flow

---
+++ Source/Networking/HaloNetworkManager.swift
@@ -0,0 +1,120 @@
+//
+//  HaloNetworkManager.swift
+//  HaloSDK
+//
+//  Created by Borja Santos-Díez on 04/08/15.
+//  Copyright © 2015 MOBGEN Technology. All rights reserved.
+//
+
+import Foundation
+import Alamofire
+
+/// Custom network manager implementation that handles authentication failures automatically,
+/// queuing the pending network operations to be re-issued after authentication
+class HaloNetworkManager: Alamofire.Manager {
+
+    /// Singleton instance of the custom network manager
+    static let instance = HaloNetworkManager()
+
+    /// Client id that identifies the client in the requests agains the API endpoints
+    var clientId: String?
+
+    /// Corresponding client secret to be provided for the API calls
+    var clientSecret: String?
+
+    typealias CompletionHandler = (NSURLRequest?, NSHTTPURLResponse?, Result<AnyObject>) -> Void
+    private typealias CachedTask = (NSURLRequest?, NSHTTPURLResponse?, Result<AnyObject>) -> Void
+
+    /// Variable that flags whether the manager is currently refreshing the auth token
+    private var isRefreshing = false
+
+    /// Queue of pending network tasks to be restarted after a successful authentication
+    private var cachedTasks = Array<CachedTask>()
+
+    /**
+    Start the request flow handling also a potential 401 response. The token will be obtained/refreshed
+    and the request will continue.
+
+    - parameter request:            Request to be performed
+    - parameter completionHandler:  Closure to be executed after the request has succeeded
+    */
+    func startRequest(request: URLRequestConvertible, completionHandler handler: CompletionHandler) -> Void {
+
+        let cachedTask: CachedTask = { [weak self] urlRequest, urlResponse, result in
+            if let strongSelf = self {
+                switch result {
+                case .Success(_):
+                    strongSelf.startRequest(request, completionHandler: handler)
+                case .Failure(_, _):
+                    handler(urlRequest, urlResponse, result)
+                }
+
+            }
+        }
+
+        if (self.isRefreshing) {
+            /// If the token is being obtained/refreshed, add the task to the queue and return
+            self.cachedTasks.append(cachedTask)
+            return
+        }
+
+        let request = self.request(request)
+
+        print(request)
+
+        request.responseJSON { [weak self] (request, response, result) -> Void in
+            if let strongSelf = self {
+                if let resp = response {
+                    if resp.statusCode == 401 {
+                        strongSelf.cachedTasks.append(cachedTask)
+                        strongSelf.refreshToken()
+                        return
+                    }
+                }
+            }
+
+            handler(request, response, result)
+        }
+    }
+
+    /**
+    Obtain/refresh an authentication token when needed
+    */
+    private func refreshToken() -> Void {
+        self.isRefreshing = true
+
+        let params: Dictionary<String, AnyObject>
+
+        if let token = Router.token {
+            params = [
+                "grant_type" : "refresh_token",
+                "client_id" : clientId!,
+                "client_secret" : clientSecret!,
+                "refresh_token" : token.refreshToken!
+            ]
+        } else {
+            params = [
+                "grant_type" : "client_credentials",
+                "client_id" : clientId!,
+                "client_secret" : clientSecret!
+            ]
+        }
+
+        self.request(Router.OAuth(params)).responseJSON { (req, resp, result) -> Void in
+            switch result {
+            case .Success(let value):
+                let dict = value as! Dictionary<String, AnyObject>
+                Router.token = Token(dict: dict)
+
+                /// Restart cached tasks
+                let cachedTaskCopy = self.cachedTasks
+                self.cachedTasks.removeAll()
+                cachedTaskCopy.map { $0(nil, nil, result) }
+            case .Failure(_, let error):
+                print("Error refreshing token: \(error.localizedDescription)")
+            }
+        }
+
+        self.isRefreshing = false
+    }
+}
--- Source/Networking/Networking.swift
+++ Source/Networking/Networking.swift
@@ -12,72 +12,28 @@ import Alamofire
 /// Module encapsulating all the networking features of the Framework
 class Networking {
 
-    private let alamofire = Alamofire.Manager.sharedInstance
+    private let net = HaloNetworkManager.instance
 
     /// Singleton instance of the networking component
     static let sharedInstance = Halo.Networking()
 
     /// Client id to be used for authentication throughout the SDK
-    var clientId: String?
-
-    /// Client secret to be used for authentication throughout the SDK
-    var clientSecret: String?
-
-    /**
-    Authenticate against the HALO backend using a client id and a client secret
-
-    - parameter completionHandler:  Closure where the response from the server can be processed
-    */
-    func authenticate(completionHandler handler: (result: Alamofire.Result<Halo.Token>) -> Void) -> Void {
-
-        if let haloToken = Router.token {
-            if haloToken.isExpired() {
-                /// Refresh token
-                haloAuthenticate(haloToken.refreshToken, completionHandler: handler)
-            } else {
-                handler(result: .Success(haloToken))
-            }
-        } else {
-            haloAuthenticate(nil, completionHandler: handler)
+    var clientId: String? {
+        set {
+            net.clientId = newValue
+        }
+        get {
+            return net.clientId
         }
     }
 
-    /**
-    Internal call to the authentication process. If a refresh token is provided, then the existing
-    token is refreshed. Otherwise, a fresh one is requested
-
-    - parameter refreshToken:       Refresh token (if any)
-    - parameter completionHandler:  Closure to be executed once the request has finished
-    */
-    private func haloAuthenticate(refreshToken: String?, completionHandler handler: (result: Alamofire.Result<Halo.Token>) -> Void) -> Void {
-
-        let params:[String: String]
-
-        if let refresh = refreshToken {
-            params = [
-                "grant_type" : "refresh_token",
-                "client_id" : clientId!,
-                "client_secret" : clientSecret!,
-                "refresh_token" : refresh
-            ]
-        } else {
-            params = [
-                "grant_type" : "client_credentials",
-                "client_id" : clientId!,
-                "client_secret" : clientSecret!
-            ]
+    /// Client secret to be used for authentication throughout the SDK
+    var clientSecret: String? {
+        set {
+            net.clientSecret = newValue
         }
-
-        alamofire.request(Router.OAuth(params)).responseJSON { (_, _, result) -> Void in
-
-            switch result {
-            case .Success(let data):
-                let token = Halo.Token(dict: data as! Dictionary<String,AnyObject>)
-                Router.token = token
-                handler(result: .Success(token))
-            case .Failure(let data, let err):
-                handler(result: .Failure(data, err))
-            }
+        get {
+            return net.clientSecret
         }
     }
 
@@ -87,49 +43,26 @@ class Networking {
     - parameter completionHandler:  Closure to be executed once the request has finished
     */
     func getModules(completionHandler handler: (result: Alamofire.Result<[Halo.HaloModule]>) -> Void) -> Void {
-
-        if let tok = Router.token {
-
-            if tok.isValid() {
-
-                alamofire.request(Router.Modules).responseJSON(completionHandler: { (_, _, result) -> Void in
-
-                    switch result {
-                    case .Success(let data):
-                        let arr = self.parseModules(data as! [Dictionary<String,AnyObject>])
-                        handler(result: .Success(arr))
-                    case .Failure(_, let error):
-                        print("Error: \(error.localizedDescription)")
-                    }
-
-                });
-            } else {
-                self.authenticate { (result) -> Void in
-                    switch (result) {
-                    case .Success(_):
-                        self.getModules(completionHandler: handler)
-                    case .Failure(_, let err):
-                        print("Error: \(err.localizedDescription)")
-                    }
+        
+        net.startRequest(Router.Modules, completionHandler: { [weak self] (req, resp, result) -> Void in
+            if let strongSelf = self {
+                switch result {
+                case .Success(let data):
+                    let arr = strongSelf.parseModules(data as! [Dictionary<String,AnyObject>])
+                    handler(result: .Success(arr))
+                case .Failure(let data, let error):
+                    handler(result: .Failure(data, error))
                 }
             }
-
-        } else {
-            authenticate { (result) -> Void in
-                switch (result) {
-                case .Success(_) :
-                    self.getModules(completionHandler: handler)
-                case .Failure(_, let err):
-                    print("Error: \(err.localizedDescription)")
-                }
-            }
-        }
+        })
     }
 
     func getModuleInstances(internalId id: String, completionHandler handler: (Alamofire.Result<[Dictionary<String, AnyObject>]>) -> Void) -> Void {
         
     }
-    
+
+    // MARK: Utility functions
+
     /**
     Parse a list of dictionaries (from the JSON response) into a list of modules
     
