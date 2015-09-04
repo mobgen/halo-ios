07bcc33dec13d43db1ec0589f4d9a0a752671596
Borja Santos-Diez <borja.santos@mobgen.com>
2015-09-04 10:27:21 +0200

    A bit more organisation in the test suite

--- Tests/CoreSpec.swift
+++ Tests/CoreSpec.swift
@@ -144,63 +144,6 @@ class CoreSpec: QuickSpec {
             
         }
 
-        describe("Requesting the list of GC instances") {
-
-            var result: Alamofire.Result<[GeneralContentInstance]>?
-
-            context("with a valid response") {
-
-                beforeEach {
-                    OHHTTPStubs.stubRequestsPassingTest({ $0.URL!.path! == "/api/generalcontent/instance/list"
-                        }, withStubResponse: { _ in
-                            let fixture = OHPathForFile("instance_list_success.json", self.dynamicType)
-                            return OHHTTPStubsResponse(fileAtPath: fixture!, statusCode: 200, headers: ["Content-Type": "application/json"])
-                    })
-
-                    waitUntil { done in
-                        mgr.generalContent.getInstancesFromObjC("dummy", success: { (userData) -> Void in
-                                result = .Success(userData)
-                                done()
-                            }, failure: nil)
-                    }
-                }
-
-                afterEach {
-                    result = nil
-                }
-
-                it("succeeds") {
-                    expect(result?.error).to(beNil())
-                }
-
-            }
-
-            context("with an invalid response") {
-
-                beforeEach {
-                    OHHTTPStubs.stubRequestsPassingTest({ $0.URL!.path! == "/api/generalcontent/instance/list"
-                        }, withStubResponse: { _ in
-                            return OHHTTPStubsResponse(JSONObject: [:], statusCode: 400, headers: ["Content-Type": "application/json"])
-                    })
-
-                    waitUntil { done in
-                        mgr.generalContent.getInstancesFromObjC("dummy", success: nil, failure: { (error) -> Void in
-                                result = Result.Failure(nil, error)
-                                done()
-                        })
-                    }
-                }
-
-                afterEach {
-                    result = nil
-                }
-
-                it("fails") {
-                    expect(result?.error).toNot(beNil());
-                }
-            }
-
-        }
     }
     
 }
