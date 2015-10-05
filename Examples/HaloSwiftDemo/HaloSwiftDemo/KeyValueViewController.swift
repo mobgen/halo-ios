c387b97dd70ecaabd7992da38f4cb26cfa89ba88
Borja Santos-Diez <borja.santos@mobgen.com>
2015-10-05 07:20:08 +0200

    Added image and YouTube previews when needed

--- Examples/HaloSwiftDemo/HaloSwiftDemo/KeyValueViewController.swift
+++ Examples/HaloSwiftDemo/HaloSwiftDemo/KeyValueViewController.swift
@@ -9,6 +9,77 @@
 import Foundation
 import UIKit
 
+class ColorCell: UITableViewCell {
+
+    var square: UIButton
+
+    init(color: UIColor, reuseIdentifier: String) {
+        square = UIButton(type: .Custom)
+        square.backgroundColor = color
+        square.enabled = false
+
+        super.init(style: .Default, reuseIdentifier: reuseIdentifier)
+
+        self.contentView.addSubview(square)
+    }
+
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    override func layoutSubviews() {
+        super.layoutSubviews()
+
+        let w = CGRectGetWidth(self.frame)
+
+        self.square.frame = CGRectMake(w - 55, 7, 40, 40)
+    }
+}
+
+class ImageCell: UITableViewCell {
+
+    var imagePreview: UIImageView
+
+    init(imageUrl: String, reuseIdentifier: String) {
+
+        imagePreview = UIImageView(frame: CGRectZero)
+        imagePreview.contentMode = .ScaleAspectFit
+
+        var url: NSURL
+
+        let regex = try! NSRegularExpression(pattern: "(?<=v(=|/))([-a-zA-Z0-9_]+)|(?<=youtu.be/)([-a-zA-Z0-9_]+)", options: .CaseInsensitive)
+
+        /// Check for a match against Youtube-like urls
+        if let match = regex.firstMatchInString(imageUrl, options: .Anchored, range: NSMakeRange(0, imageUrl.characters.count)) {
+            let videoId = (imageUrl as NSString).substringWithRange(match.rangeAtIndex(0))
+            url = NSURL(string: "http://img.youtube.com/vi/\(videoId)/default.jpg")!
+        } else {
+            url = NSURL(string: imageUrl)!
+        }
+
+        imagePreview.af_setImageWithURL(url)
+
+        super.init(style: .Default, reuseIdentifier: reuseIdentifier)
+
+        self.contentView.addSubview(imagePreview)
+    }
+
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    override func layoutSubviews() {
+        super.layoutSubviews()
+
+        let w = CGRectGetWidth(self.frame)
+        let h = CGRectGetHeight(self.frame)
+
+        self.imagePreview.frame = CGRectMake(w - 115, 0, 100, h)
+    }
+
+
+}
+
 class KeyValueViewController: UITableViewController {
 
     var values: [(String, String?)] = []
@@ -41,18 +112,45 @@ class KeyValueViewController: UITableViewController {
         return values.count
     }
 
+    override func tableView(tableView: UITableView, heightForRowAtIndexPath indexPath: NSIndexPath) -> CGFloat {
+        return 54
+    }
+
     override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
-
-        var cell = tableView.dequeueReusableCellWithIdentifier(cellIdent)
-
-        if (cell == nil) {
-            cell = UITableViewCell(style: UITableViewCellStyle.Value1, reuseIdentifier: cellIdent)
-        }
 
         let tuple = values[indexPath.row]
 
-        cell?.textLabel?.text = tuple.0
-        cell?.detailTextLabel?.text = tuple.1
+        let title = tuple.0
+        let value = tuple.1!
+
+        var cell: UITableViewCell?
+
+        if value.hasPrefix("#") {
+            cell = tableView.dequeueReusableCellWithIdentifier("colorCell")
+
+            if cell == nil {
+                cell = ColorCell(color: UIColor(rgba: value), reuseIdentifier: "colorCell")
+            }
+
+        } else if value.containsString("www.") {
+
+            cell = tableView.dequeueReusableCellWithIdentifier("imageCell")
+
+            if cell == nil {
+                cell = ImageCell(imageUrl: value, reuseIdentifier: "imageCell")
+            }
+
+        } else {
+            cell = tableView.dequeueReusableCellWithIdentifier(cellIdent)
+
+            if (cell == nil) {
+                cell = UITableViewCell(style: UITableViewCellStyle.Value1, reuseIdentifier: cellIdent)
+            }
+
+            cell?.detailTextLabel?.text = value
+        }
+
+        cell?.textLabel?.text = title
 
         return cell!
 
