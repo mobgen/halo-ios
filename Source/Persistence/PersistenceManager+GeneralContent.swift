eed7d12327613fa6731ad6c1bdcfac714deb9122
Borja Santos-Diez <borja.santos@mobgen.com>
2016-03-08 19:14:54 +0100

    More persistence layer code

--- Source/Persistence/PersistenceManager+GeneralContent.swift
+++ Source/Persistence/PersistenceManager+GeneralContent.swift
@@ -64,6 +64,7 @@ extension PersistenceManager {
                         handler?(.Failure(error))
                     }
                 default:
+                    NSLog("Wrong response from server")
                     break
                 }
             }
@@ -95,23 +96,28 @@ extension PersistenceManager {
             return
         }
         
-//        Manager.network.generalContentInstance(instanceId).response { (result) -> Void in
-//            switch result {
-//            case .Success(let instance, _):
-//                handler?(result)
-//                
-//                try! self.realm.write({ () -> Void in
-//                    self.realm.add(PersistentGeneralContentInstance(instance), update: true)
-//                })
-//            case .Failure(let error):
-//                if error.code == -1009 {
-//                    self.getInstanceLocalDataDontLoad(instanceId, completionHandler: handler)
-//                } else {
-//                    //handler?(result)
-//                }
-//            }
-//        }
-        
+        Manager.network.generalContentInstance(instanceId).response { (result) -> Void in
+            switch result {
+            case .Success(let data as [String : AnyObject], _):
+                
+                let instance = GeneralContentInstance(data)
+                
+                handler?(.Success(instance, false))
+                
+                try! self.realm.write({ () -> Void in
+                    self.realm.add(PersistentGeneralContentInstance(instance), update: true)
+                })
+            case .Failure(let error):
+                if error.code == -1009 {
+                    self.getInstanceLocalDataDontLoad(instanceId, completionHandler: handler)
+                } else {
+                    handler?(.Failure(error))
+                }
+            default:
+                NSLog("Wrong response from server")
+                break
+            }
+        }
     }
     
     private func getInstanceLocalDataDontLoad(instanceId: String, completionHandler handler: ((Halo.Result<Halo.GeneralContentInstance, NSError>) -> Void)?) -> Void {
@@ -137,29 +143,48 @@ extension PersistenceManager {
                 return
             }
             
-//            Manager.network.generalContentInstances(instanceIds: instanceIds, flags: flags).response { (result) -> Void in
-//                switch result {
-//                case .Success(let instances, _):
-//                    
-//                    handler?(result)
-//                    
-//                    try! self.realm.write({ () -> Void in
-//                        
-//                        self.realm.delete(self.realm.objects(PersistentGeneralContentInstance).filter("id IN %@", instanceIds))
-//                        
-//                        for instance in instances {
-//                            self.realm.add(PersistentGeneralContentInstance(instance), update: true)
-//                        }
-//                    })
-//                    
-//                case .Failure(let error):
-//                    if error.code == -1009 {
-//                        self.getInstancesByIdsLocalDataDontLoad(instanceIds, completionHandler: handler)
-//                    } else {
-//                        handler?(result)
-//                    }
-//                }
-//            }
+            Manager.network.generalContentInstances(instanceIds: instanceIds, flags: flags).response { (result) -> Void in
+                switch result {
+                case .Success(let data as [[String : AnyObject]], _):
+                    
+                    let instances = data.map({ GeneralContentInstance($0) })
+                    
+                    handler?(.Success(instances, false))
+                    
+                    try! self.realm.write({ () -> Void in
+                        
+                        self.realm.delete(self.realm.objects(PersistentGeneralContentInstance).filter("id IN %@", instanceIds))
+                        
+                        for instance in instances {
+                            self.realm.add(PersistentGeneralContentInstance(instance), update: true)
+                        }
+                    })
+                case .Success(let data as [String : AnyObject], _):
+                    
+                    let items = data["items"] as! [[String : AnyObject]]
+                    let instances = items.map({ GeneralContentInstance($0) })
+                    
+                    handler?(.Success(instances, false))
+                    
+                    try! self.realm.write({ () -> Void in
+                        
+                        self.realm.delete(self.realm.objects(PersistentGeneralContentInstance).filter("id IN %@", instanceIds))
+                        
+                        for instance in instances {
+                            self.realm.add(PersistentGeneralContentInstance(instance), update: true)
+                        }
+                    })
+                case .Failure(let error):
+                    if error.code == -1009 {
+                        self.getInstancesByIdsLocalDataDontLoad(instanceIds, completionHandler: handler)
+                    } else {
+                        handler?(.Failure(error))
+                    }
+                default:
+                    NSLog("Wrong response from server")
+                    break
+                }
+            }
     }
     
     private func getInstancesByIdsLocalDataDontLoad(instanceIds: [String], completionHandler handler: ((Halo.Result<[Halo.GeneralContentInstance], NSError>) -> Void)?) -> Void {
