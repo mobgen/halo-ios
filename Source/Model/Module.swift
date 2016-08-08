ca7b174efe2dbded88a39c0470c8ce225a424ac9
Borja Santos-Diez <borja.santos@mobgen.com>
2016-08-08 13:32:26 +0200

    Added constants for module JSON keys

--- Source/Model/Module.swift
+++ Source/Model/Module.swift
@@ -9,26 +9,38 @@
 import Foundation
 
 /**
-Model class representing the different modules available in Halo
-*/
+ Model class representing the different modules available in Halo
+ */
 
 public struct Module {
+    
+    public static let kCustomer = "customer"
+    public static let kId = "id"
+    public static let kInternalId = "internalId"
+    public static let kName = "name"
+    public static let kModuleType = "moduleType"
+    public static let kEnabled = "enabled"
+    public static let kIsSingle = "isSingle"
+    public static let kCreatedAt = "createdAt"
+    public static let kUpdatedAt = "updatedAt"
+    public static let kUpdatedBy = "updatedBy"
+    public static let kTags = "tags"
     
     /// Identifier of the customer
     public internal(set) var customerId: Int?
     
     /// Unique identifier of the module
     public internal(set) var id: Int?
-
+    
     /// Internal id of the module
     public internal(set) var internalId: String?
     
     /// Visual name of the module
     public internal(set) var name: String?
-
+    
     /// Type of the module
     public internal(set) var type: Halo.ModuleType?
-
+    
     /// Identifies the module as enabled or not
     public internal(set) var enabled: Bool = true
     
@@ -40,7 +52,7 @@ public struct Module {
     
     /// Date of the last update performed in this module
     public internal(set) var updatedAt: NSDate?
-
+    
     /// Name of the user who updated the module in last place
     public internal(set) var updatedBy: String?
     
@@ -48,21 +60,22 @@ public struct Module {
     public internal(set) var tags: [String: Halo.Tag] = [:]
     
     /**
-    Initialise a HaloModule from a dictionary
-    
-    - parameter dict:   Dictionary containing the information about the module
-    */
+     Initialise a HaloModule from a dictionary
+     
+     - parameter dict:   Dictionary containing the information about the module
+     */
     public init(_ dict: [String:AnyObject]) {
-        id = dict["id"] as? Int
-        customerId = dict["customer"] as? Int
-        internalId = dict["internalId"] as? String
-        name = dict["name"] as? String
-        isSingle = dict["isSingle"] as? Bool ?? false
-        enabled = dict["enabled"] as? Bool ?? false
-        updatedBy = dict["updatedBy"] as? String
+        
+        id = dict[Module.kId] as? Int
+        customerId = dict[Module.kCustomer] as? Int
+        internalId = dict[Module.kInternalId] as? String
+        name = dict[Module.kName] as? String
+        isSingle = dict[Module.kIsSingle] as? Bool ?? false
+        enabled = dict[Module.kEnabled] as? Bool ?? false
+        updatedBy = dict[Module.kUpdatedBy] as? String
         tags = [:]
         
-        if let tagsList = dict["tags"] as? [[String: AnyObject]] {
+        if let tagsList = dict[Module.kTags] as? [[String: AnyObject]] {
             tags = tagsList.map({ (dict) -> Halo.Tag in
                 return Halo.Tag.fromDictionary(dict)
             }).reduce([:], combine: { (dict, tag: Halo.Tag) -> [String: Halo.Tag] in
@@ -72,15 +85,15 @@ public struct Module {
             })
         }
         
-        if let moduleTypeDict = dict["moduleType"] as? [String: AnyObject] {
+        if let moduleTypeDict = dict[Module.kModuleType] as? [String: AnyObject] {
             type = ModuleType(moduleTypeDict)
         }
-
-        if let created = dict["createdAt"] as? Double {
+        
+        if let created = dict[Module.kCreatedAt] as? Double {
             createdAt = NSDate(timeIntervalSince1970: created/1000)
         }
         
-        if let updated = dict["updatedAt"] as? Double {
+        if let updated = dict[Module.kUpdatedAt] as? Double {
             updatedAt = NSDate(timeIntervalSince1970: updated/1000)
         }
     }
