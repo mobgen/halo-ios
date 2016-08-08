04dc84a1db42d3238be650f978a9ebb90699913a
Borja Santos-Diez <borja.santos@mobgen.com>
2016-08-08 11:00:34 +0200

    Change visibility of some properties of the request

--- Source/Networking/Request.swift
+++ Source/Networking/Request.swift
@@ -20,15 +20,16 @@ public protocol Requestable {
 
 public class Request<T>: Requestable, CustomDebugStringConvertible {
 
-    public private(set) var url: NSURL?
-    public private(set) var include = false
-    public private(set) var method: Halo.Method = .GET
-    public private(set) var parameterEncoding: Halo.ParameterEncoding = .URL
-    public private(set) var headers: [String: String] = [:]
-    public private(set) var offlinePolicy = Manager.core.defaultOfflinePolicy
-    public private(set) var params: [String: AnyObject] = [:]
-    public private(set) var authenticationMode: Halo.AuthenticationMode = .App
-    public private(set) var responseParser: ((AnyObject) -> T?)?
+    private var url: NSURL?
+    private var include = false
+    private var method: Halo.Method = .GET
+    private var parameterEncoding: Halo.ParameterEncoding = .URL
+    private var headers: [String: String] = [:]
+    private var offlinePolicy = Manager.core.defaultOfflinePolicy
+    private var params: [String: AnyObject] = [:]
+    private var responseParser: ((AnyObject) -> T?)?
+    
+    public var authenticationMode: Halo.AuthenticationMode = .App
     
     public var URLRequest: NSMutableURLRequest {
         let req = NSMutableURLRequest(URL: self.url!)
