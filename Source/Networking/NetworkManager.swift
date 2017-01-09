674d53d24728da3571dba480d6b351fc942da7be
Borja Santos-Diez <borja.santos@mobgen.com>
2017-01-09 14:34:56 +0100

    Added new certificate and enable SSL pinning by default

--- Source/Networking/NetworkManager.swift
+++ Source/Networking/NetworkManager.swift
@@ -64,7 +64,7 @@ open class NetworkManager: NSObject, HaloManager, URLSessionDelegate {
         if let path = bundle.path(forResource: Manager.core.configuration, ofType: "plist") {
 
             if let data = NSDictionary(contentsOfFile: path) {
-                let disable = data[CoreConstants.disableSSLpinning] as? Bool ?? true
+                let disable = data[CoreConstants.disableSSLpinning] as? Bool ?? false
                 self.enableSSLpinning = !disable
             }
         }
