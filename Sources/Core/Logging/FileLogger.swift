3f05fea5e434da0572d6eb4ee8b7c55c33559ccf
Borja Santos-Diez <borja.santos@mobgen.com>
2017-01-25 17:51:46 +0100

    Added date and time for the FileLogger

--- Sources/Core/Logging/FileLogger.swift
+++ Sources/Core/Logging/FileLogger.swift
@@ -13,24 +13,31 @@ open class FileLogger: NSObject, Logger {
     
     let dir: URL? = try? FileManager.default.url(for: .documentDirectory, in: .userDomainMask, appropriateFor: nil, create: false)
     public var filePath: URL?
+    let formatter = DateFormatter()
     
     override public init() {
         super.init()
+        formatter.dateFormat = "yyyy/MM/dd HH:mm:ss.SSS"
         let filename = "\(Bundle.main.object(forInfoDictionaryKey: "CFBundleDisplayName"))-\(NSDate().timeIntervalSince1970).txt"
         filePath = dir?.appendingPathComponent(filename)
     }
     
     public func logMessage(message: String, level: LogLevel) {
         
-        if let file = filePath, let data = "\(message)\n".data(using: .utf8) {
-            if FileManager.default.fileExists(atPath: file.path),
-                let fileHandle = FileHandle(forWritingAtPath: file.path) {
-                
-                fileHandle.seekToEndOfFile()
-                fileHandle.write(data)
-                fileHandle.closeFile()
-            } else {
-                try? data.write(to: file, options: .atomic)
+        if let file = filePath {
+            
+            let date = formatter.string(from: Date())
+            
+            if let data = "[\(date)] \(message)\n".data(using: .utf8) {
+                if FileManager.default.fileExists(atPath: file.path),
+                    let fileHandle = FileHandle(forWritingAtPath: file.path) {
+                    
+                    fileHandle.seekToEndOfFile()
+                    fileHandle.write(data)
+                    fileHandle.closeFile()
+                } else {
+                    try? data.write(to: file, options: .atomic)
+                }
             }
         }
     }
