f117b9976e449004e3e64c057ef30bc730834bcf
Borja Santos-Díez <borja.santos@mobgen.com>
2017-08-24 09:33:08 +0200

    Revert the HaloAppDelegate to a regular class

--- Sources/Core/HaloAppDelegate.swift
+++ Sources/Core/HaloAppDelegate.swift
@@ -10,15 +10,13 @@ import Foundation
 import UIKit
 
 /// Helper class intended to be used as superclass by any AppDelegate (Swift only)
-@objc
-public protocol HaloAppDelegate {}
-
-extension HaloAppDelegate {
-    public func application(_ application: UIApplication, willFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey : Any]? = nil) -> Bool {
+open class HaloAppDelegate: UIResponder, UIApplicationDelegate {
+    
+    open func application(_ application: UIApplication, willFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey : Any]? = nil) -> Bool {
         return Manager.core.applicationWillFinishLaunching(application)
     }
     
-    public func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey : Any]? = nil) -> Bool {
+    open func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey : Any]? = nil) -> Bool {
         return Manager.core.applicationDidFinishLaunching(application, didFinishLaunchingWithOptions: launchOptions)
     }
     
@@ -28,7 +26,7 @@ extension HaloAppDelegate {
     - parameter application: Application being configured
     - parameter deviceToken: Device token obtained in previous steps
     */
-    public func application(_ application: UIApplication, didRegisterForRemoteNotificationsWithDeviceToken deviceToken: Data) {
+    open func application(_ application: UIApplication, didRegisterForRemoteNotificationsWithDeviceToken deviceToken: Data) {
         Manager.core.application(application, didRegisterForRemoteNotificationsWithDeviceToken: deviceToken)
     }
 
@@ -38,28 +36,28 @@ extension HaloAppDelegate {
      - parameter application: Application being configured
      - parameter error:       Error thrown during the process
      */
-    public func application(_ application: UIApplication, didFailToRegisterForRemoteNotificationsWithError error: Error) {
+    open func application(_ application: UIApplication, didFailToRegisterForRemoteNotificationsWithError error: Error) {
         Manager.core.application(application, didFailToRegisterForRemoteNotificationsWithError: error as NSError)
     }
 
-    public func applicationDidBecomeActive(_ application: UIApplication) {
+    open func applicationDidBecomeActive(_ application: UIApplication) {
         Manager.core.applicationDidBecomeActive(application)
     }
 
-    public func applicationDidEnterBackground(_ application: UIApplication) {
+    open func applicationDidEnterBackground(_ application: UIApplication) {
         Manager.core.applicationDidEnterBackground(application)
     }
 
-    public func application(_ application: UIApplication, didReceiveRemoteNotification userInfo: [AnyHashable: Any]) {
+    open func application(_ application: UIApplication, didReceiveRemoteNotification userInfo: [AnyHashable: Any]) {
         // Triggered when there is user interaction
         Manager.core.application(application, didReceiveRemoteNotification: userInfo as [NSObject : AnyObject], userInteraction: true)
     }
     
-    public func application(_ app: UIApplication, open url: URL, options: [UIApplicationOpenURLOptionsKey : Any] = [:]) -> Bool {
+    open func application(_ app: UIApplication, open url: URL, options: [UIApplicationOpenURLOptionsKey : Any] = [:]) -> Bool {
         return Manager.core.application(app, open: url, options: options)
     }
     
-    public func application(_ application: UIApplication, open url: URL, sourceApplication: String?, annotation: Any) -> Bool {
+    open func application(_ application: UIApplication, open url: URL, sourceApplication: String?, annotation: Any) -> Bool {
         return Manager.core.application(application, open: url, sourceApplication: sourceApplication, annotation: annotation)
     }
 
@@ -70,7 +68,7 @@ extension HaloAppDelegate {
      - parameter userInfo:          Dictionary containing all the information about the notification
      - parameter completionHandler: Handler to be executed once the fetch has finished
      */
-    public func application(_ application: UIApplication, didReceiveRemoteNotification userInfo: [AnyHashable: Any], fetchCompletionHandler completionHandler: @escaping (UIBackgroundFetchResult) -> Void) {
+    open func application(_ application: UIApplication, didReceiveRemoteNotification userInfo: [AnyHashable: Any], fetchCompletionHandler completionHandler: @escaping (UIBackgroundFetchResult) -> Void) {
         Manager.core.application(application, didReceiveRemoteNotification: userInfo as [NSObject : AnyObject], userInteraction: application.applicationState == .inactive, fetchCompletionHandler: completionHandler)
     }
 
